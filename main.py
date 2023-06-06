fd4f82a7c011fa9976baff8b58a4054b19f681e1
Indrani11 <110850992+Indrani11@users.noreply.github.com>
2023-06-06 13:17:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+from tkinter import *
+from tkinter import messagebox
+import time
+import sys
+from pygame import mixer
+from PIL import Image, ImageTk
+
+
+def alarm():
+    alarm_time = user_input.get()
+    if alarm_time == "":
+        messagebox.askretrycancel("Error Message", "Please Enter Value")
+    else:
+        while True:
+            time.sleep(1)
+            if (alarm_time == time.strftime("%H:%M")):
+                playmusic()
+
+
+def playmusic():
+    mixer.init()
+    mixer.music.load('Alarm-Windows-10.mp3')
+    mixer.music.play()
+    while mixer.music.get_busy():
+        time.sleep(10)
+        mixer.music.stop()
+        sys.exit()
+
+
+root = Tk()
+root.title("Alarm Clock")
+root.geometry("600x380")
+canvas = Canvas(root, width=600, height=380)
+image = ImageTk.PhotoImage(Image.open("alarm_clock.jpg"))
+canvas.create_image(0, 0, anchor=NW, image=image)
+canvas.pack()
+header = Frame(root)
+
+
+box1 = Frame(root)
+box1.place(x=300, y=200)
+box2 = Frame(root)
+box2.place(x=300, y=260)
+
+user_input = Entry(box1, font=('Arial', 20), width=8)
+user_input.grid(row=0, column=2)
+
+start_button = Button(box2, text="Set Alarm", font=(
+    'Arial', 16, 'bold'), command=alarm)
+start_button.grid(row=2, column=2)
+root.mainloop()
